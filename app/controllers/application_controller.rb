09015e78f494492040578eabb0b09565e982d2c9
PuZZleDucK <PuZZleDucK@gmail.com>
2025-05-18 02:10:44 +1000

    Add navbar with user selector and adjust chat history

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_current_user
+
+  def select_user
+    session[:user_id] = params[:user_id]
+    redirect_back fallback_location: root_path
+  end
+
+  private
+
+  def set_current_user
+    @current_user = User.find_by(id: session[:user_id])
+    @current_user ||= User.first
+  end
 end
