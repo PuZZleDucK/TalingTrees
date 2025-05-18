1bfc5784d430726c849310fc237e58d47fc2e426
PuZZleDucK <PuZZleDucK@gmail.com>
2025-05-18 17:26:35 +1000

    Add Chat and Message models with persistence

--- app/controllers/chats_controller.rb
+++ app/controllers/chats_controller.rb
@@ -4,7 +4,20 @@ class ChatsController < ApplicationController
   def create
     tree = Tree.find(params[:id])
     history = params[:history].is_a?(String) ? JSON.parse(params[:history]) : params[:history]
-    messages = [{ "role" => "system", "content" => tree.llm_sustem_prompt.to_s }] + Array(history)
+
+    chat = if params[:chat_id].present?
+      Chat.find(params[:chat_id])
+    else
+      Chat.create!(user: @current_user, tree: tree).tap do |c|
+        response.headers['X-Chat-Id'] = c.id.to_s
+      end
+    end
+
+    Array(history).each do |msg|
+      chat.messages.create!(role: msg['role'], content: msg['content'])
+    end
+
+    messages = [{ 'role' => 'system', 'content' => tree.llm_sustem_prompt.to_s }] + Array(history)
 
     response.headers['Content-Type'] = 'text/event-stream'
     client = Ollama.new(
@@ -16,12 +29,18 @@ class ChatsController < ApplicationController
       }
     )
 
+    assistant_content = ''
+
     begin
       client.chat({ model: tree.llm_model, messages: messages }) do |chunk = nil, _raw = nil|
         content = chunk&.dig('message', 'content')
-        response.stream.write(content.to_s) if content
+        if content
+          assistant_content << content
+          response.stream.write(content)
+        end
       end
     ensure
+      chat.messages.create!(role: 'assistant', content: assistant_content)
       response.stream.close
     end
   end
