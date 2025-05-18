9731ee0a7c519d04492cdc6ff88e889e4b85a4c4
PuZZleDucK <PuZZleDucK@gmail.com>
2025-05-18 13:27:44 +1000

    Add streaming chat integration with Ollama

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'trees#index'
   resources :trees, only: [:index]
+  post 'trees/:id/chat', to: 'chats#create', as: 'tree_chat'
   post 'select_user', to: 'application#select_user'
 end
