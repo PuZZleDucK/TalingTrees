649885e87cca792bccf982b81de40e4c6b669e3a
PuZZleDucK <puzzleduck@gmail.com>
2025-05-24 21:22:51 +1000

    uopdate deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,10 @@ ollama pull Qwen3:0.6b
 # 3) Now run all your Rails/Rake setup
 bundle exec rails db:migrate
 bundle exec rails db:seed
-bundle exec rake db:import_trees[15]
-bundle exec rake db:name_trees
-bundle exec rake db:add_relationships
-bundle exec rake db:system_prompts
+bundle exec rake db:import_trees
+# bundle exec rake db:name_trees
+# bundle exec rake db:add_relationships
+# bundle exec rake db:system_prompts
 
 # 4) Finally, launch Rails
 exec bundle exec rails server -b 0.0.0.0
