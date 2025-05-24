326ca1f8b5e13ad361872bb5df3af82a92c3b9af
PuZZleDucK <puzzleduck@gmail.com>
2025-05-25 01:04:38 +1000

    fix config

--- config/application.rb
+++ config/application.rb
@@ -6,31 +6,29 @@ require 'rails/all'
 
 Bundler.require(*Rails.groups)
 
-Rails.application.configure do
-  # remove www-static stack
-  config.middleware.delete Rack::Sendfile
-  config.middleware.delete ActionDispatch::Static
-
-  # remove session/cookie stack
-  config.middleware.delete ActionDispatch::Cookies
-  config.middleware.delete ActionDispatch::Session::CookieStore
-  config.middleware.delete ActionDispatch::Flash
-
-  # remove exception rendering
-  # config.middleware.delete ActionDispatch::ShowExceptions
-  config.middleware.delete ActionDispatch::DebugExceptions
-
-  # remove policies if you’re not using them
-  config.middleware.delete ActionDispatch::ContentSecurityPolicy::Middleware
-  config.middleware.delete ActionDispatch::PermissionsPolicy::Middleware
-
-  # remove temp file cleanup
-  # config.middleware.delete Rack::TempfileReaper
-end
-
 module TalingTrees
   # Main Rails application class.
   class Application < Rails::Application
     config.load_defaults 7.1
+
+    # remove www-static stack
+    config.middleware.delete Rack::Sendfile
+    config.middleware.delete ActionDispatch::Static
+
+    # remove session/cookie stack
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+    config.middleware.delete ActionDispatch::Flash
+
+    # remove exception rendering
+    # config.middleware.delete ActionDispatch::ShowExceptions
+    config.middleware.delete ActionDispatch::DebugExceptions
+
+    # remove policies if you’re not using them
+    config.middleware.delete ActionDispatch::ContentSecurityPolicy::Middleware
+    config.middleware.delete ActionDispatch::PermissionsPolicy::Middleware
+
+    # remove temp file cleanup
+    # config.middleware.delete Rack::TempfileReaper
   end
 end
