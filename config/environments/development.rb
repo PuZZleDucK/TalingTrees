f6034651cd21b012d533a81a54b391bbf5bc558b
PuZZleDucK <PuZZleDucK@gmail.com>
2025-05-24 13:29:30 +1000

    Add Active Storage configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,4 +6,5 @@ Rails.application.configure do
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true
+  config.active_storage.service = :local
 end
