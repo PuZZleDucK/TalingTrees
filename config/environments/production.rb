0f27710f010b6c60fc86f664be59955005949a29
PuZZleDucK <PuZZleDucK@gmail.com>
2025-05-24 13:15:08 +1000

    Add environment configs for test and production

---
+++ config/environments/production.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+Rails.application.configure do
+  # Code is not reloaded between requests.
+  config.cache_classes = true
+
+  # Eager load code on boot. This eager loads most of Rails and
+  # your application in memory, allowing both threaded web servers
+  # and those relying on copy on write to perform better.
+  config.eager_load = true
+
+  # Ensure secret_key_base is set for verifying signed cookies.
+  config.secret_key_base = ENV["SECRET_KEY_BASE"] if ENV["SECRET_KEY_BASE"]
+end
