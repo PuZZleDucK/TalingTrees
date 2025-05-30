9e58121cb1c4a4db614bfbe27249bb3784d7a83f
PuZZleDucK <PuZZleDucK@gmail.com>
2025-05-31 00:26:32 +1000

    [FIX] brighten suburb boundaries

--- app/models/suburb.rb
+++ app/models/suburb.rb
@@ -1,4 +1,35 @@
 # frozen_string_literal: true
 
+# Represents a suburb boundary polygon.
+require 'rgeo'
 class Suburb < ApplicationRecord
+  # Returns an array of polygons representing the suburb boundary.
+  # Each polygon is an array of [lat, lon] coordinate pairs.
+  def polygons
+    require 'rgeo' unless defined?(::RGeo)
+
+    geom = boundary
+    unless geom.respond_to?(:geometry_type)
+      factory = ::RGeo::Geographic.spherical_factory(srid: 4326)
+      begin
+        geom = ::RGeo::WKRep::WKTParser.new(factory, support_ewkt: true).parse(boundary.to_s)
+      rescue ::RGeo::Error::ParseError
+        return []
+      end
+    end
+
+    if geom.geometry_type == ::RGeo::Feature::MultiPolygon
+      geom.map { |poly| ring_coords(poly.exterior_ring) }
+    elsif geom.geometry_type == ::RGeo::Feature::Polygon
+      [ring_coords(geom.exterior_ring)]
+    else
+      []
+    end
+  end
+
+  private
+
+  def ring_coords(ring)
+    ring.points.map { |p| [p.y, p.x] }
+  end
 end
