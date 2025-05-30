0da0fee443a71e26e020e7c1d427838d551280a0
PuZZleDucK <puzzleduck@gmail.com>
2025-05-31 01:14:23 +1000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_06_13_000000) do
+ActiveRecord::Schema[7.2].define(version: 2025_06_14_000000) do
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tree_id"
