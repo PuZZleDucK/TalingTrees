e895af8bd8dde0a181fa11eeeda42bd1b0c0fc81
Ben Minerds <puzzleduck@gmail.com>
2025-10-06 22:23:05 +1100

    CHORE: install GEOS for Render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+sudo apt-get update -y
+sudo apt-get install -y --no-install-recommends libgeos-dev
+
 bundle install --without development test
 yarn install --frozen-lockfile
 bundle exec rails assets:precompile
